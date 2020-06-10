de3a99e8294d4d953f25bb8f53f4de708ca0a96f
mmosc <ma.moscati@gmail.com>
2020-06-10 07:38:16 +0000

    modify readme and delete queries

--- create_tables.py
+++ create_tables.py
@@ -4,21 +4,37 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    '''
+    Drop all tables in the DB
+    
+    Arguments:
+    cur  -- cursor
+    conn -- connection to the DB
+    '''
     for query in drop_table_queries:
+        print("Executing: ",query)
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    '''
+    Drop all tables in the DB
+    
+    Arguments:
+    cur  -- cursor
+    conn -- connection to the DB
+    '''
     for query in create_table_queries:
+        print("Executing: ",query)
         cur.execute(query)
         conn.commit()
 
 
 def main():
     config = configparser.ConfigParser()
-    config.read('dwh.cfg')
-
+    config.read_file(open('dwh.cfg'))
+    
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
 
